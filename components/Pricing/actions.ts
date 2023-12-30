37fd32a6e98051e4c62f64d2da439a8a0ece1731
shahreaz0 <ashahreaz@gmail.com>
2023-12-30 23:59:16 +0600

    Set redirect url for prod

--- components/Pricing/actions.ts
+++ components/Pricing/actions.ts
@@ -2,6 +2,13 @@
 
 import { stripe } from "@/configs/stripe";
 
+console.log(process.env.NODE_ENV);
+
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://ctrd-admin.vercel.app";
+
 export async function getPrice(priceId: string) {
   const session = await stripe.checkout.sessions.create({
     line_items: [
@@ -11,8 +18,16 @@ export async function getPrice(priceId: string) {
       },
     ],
     mode: "payment",
-    success_url: "http://localhost:3000/payment/success",
-    cancel_url: "http://localhost:3000",
+    success_url: `${BASE_URL}/payment/success`,
+    cancel_url: `${BASE_URL}`,
+    invoice_creation: {
+      enabled: true,
+    },
+    customer_creation: "always",
+    phone_number_collection: {
+      enabled: true,
+    },
+    billing_address_collection: "required",
   });
 
   return session.url;
