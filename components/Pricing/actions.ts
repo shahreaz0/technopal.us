363b265f8c42c2f4af048719272f131a936e0aa9
shahreaz0 <ashahreaz@gmail.com>
2024-04-20 05:23:52 +0600

    feat: set email in stripe checkout session

--- components/Pricing/actions.ts
+++ components/Pricing/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { stripe } from "@/configs/stripe";
+import { clerkClient } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs";
 
 const BASE_URL =
   process.env.NODE_ENV === "development"
@@ -8,6 +10,17 @@ const BASE_URL =
     : "https://technopal-us.vercel.app";
 
 export async function getPrice(priceId: string) {
+  const { userId } = await auth();
+
+  let email = "";
+  if (userId) {
+    const { emailAddresses } = await clerkClient.users.getUser(userId);
+
+    email = emailAddresses[0].emailAddress;
+  }
+
+  console.log({ userId });
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -22,6 +35,7 @@ export async function getPrice(priceId: string) {
       enabled: true,
     },
     customer_creation: "always",
+    customer_email: email,
     phone_number_collection: {
       enabled: true,
     },
